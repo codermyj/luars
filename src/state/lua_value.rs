cf26ae4fc43fbfff63811d29bedfba6e684ae8c8
codermyj <lightmyj@163.com>
2023-01-06 15:27:19 +0800

    实现state和api模块

---
+++ src/state/lua_value.rs
@@ -0,0 +1,21 @@
+use crate::api::consts::*;
+
+pub enum LuaValue {
+    Nil,
+    Bool(bool),
+    Integer(i64),
+    Number(f64),
+    String(String),
+}
+
+impl LuaValue {
+    fn type_of(&self) -> u8 {
+        match self {
+            LuaValue::Nil => LUA_TNIL,
+            LuaValue::Bool(b) => LUA_TBOOLEAN,
+            LuaValue::Integer(i) => LUA_TNUMBER,
+            LuaValue::Number(f) => LUA_TNUMBER,
+            LuaValue::String(s) => LUA_TSTRING,
+        }
+    }
+}
